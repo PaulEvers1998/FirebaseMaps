3737aa1ef57df26ed7bd45f26baa3dd502184f15
Paul <paulusman@live.nl>
2018-11-17 18:36:40 +0100

    Includes Google Auth

--- public/app.js
+++ public/app.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", event => {
     console.log("app.js loaded");
 })
+
+function signInGoogle(){
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider)
+    .then(result => {
+            const user = result.user;
+            document.getElementById("login").remove();
+    });
+}
